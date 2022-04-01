5e2b9d90c57bb71589c29180fcd34b48d486718d
Simone Serpi <simoneserpi@hotmail.it>
2022-04-01 16:17:00 +0200

    Fine_Bonus

--- CibiPreferiti/Program.cs
+++ CibiPreferiti/Program.cs
@@ -1,18 +1,34 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Benvenuto!");
-string[] cibi = { "Pizza", "Pasta", "Ostrica", "Sushi", "Raffaello", "Spinaci" };
+string[] cibi = { "Pizza", "Pasta", "Ostrica", "Sushi", "Raffaello", "Spinaci", "Broccoli" };
 
 
 
-Console.WriteLine("la classifica è composta da " + cibi.Length + "posizioni");
+Console.WriteLine("la classifica è composta da " + cibi.Length + " posizioni");
 
-for (int i = 0; i < cibi.Length; i++)
+for ( int i = 0; i < cibi.Length; i++)
 {
-    Console.WriteLine(cibi[i] + " è alla posizione " + i ); 
+    int posizione = i + 1;
+    Console.WriteLine(cibi[i] + " è alla posizione " + posizione ); 
    // Console.WriteLine("la classifica è composta da " [i] + "posizioni");
 }
-
 
 Console.WriteLine("Il tuo cibo preferito è " + cibi[1]);
 int cibi2 = cibi.Length - 1;
 Console.WriteLine("il tuo cibo meno preferito è " + cibi[cibi2]);
+
+int metaClass = cibi.Length%2;
+
+if (metaClass==0)
+{
+    metaClass = cibi.Length;
+    Console.WriteLine(metaClass);
+    metaClass = metaClass / 2;
+    Console.WriteLine("il cibo medio è " + cibi[metaClass]);
+    Console.WriteLine("il cibo medio è " + cibi[metaClass-1]);
+}else
+{
+    metaClass = cibi.Length;
+    metaClass = metaClass / 2;
+    Console.WriteLine("il cibo medio è " + cibi[metaClass]);
+}
