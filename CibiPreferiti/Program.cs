fe45a8ab13677b5229038e29b7092e7c055e41cd
Simone Serpi <simoneserpi@hotmail.it>
2022-04-01 15:20:39 +0200

    Fine_Base

--- CibiPreferiti/Program.cs
+++ CibiPreferiti/Program.cs
@@ -1,2 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Benvenuto!");
+string[] cibi = { "Pizza", "Pasta", "Ostrica", "Sushi", "Raffaello", "Spinaci" };
+
+
+
+Console.WriteLine("la classifica è composta da " + cibi.Length + "posizioni");
+
+for (int i = 0; i < cibi.Length; i++)
+{
+    Console.WriteLine(cibi[i] + " è alla posizione " + i ); 
+   // Console.WriteLine("la classifica è composta da " [i] + "posizioni");
+}
+
+
+Console.WriteLine("Il tuo cibo preferito è " + cibi[1]);
+int cibi2 = cibi.Length - 1;
+Console.WriteLine("il tuo cibo meno preferito è " + cibi[cibi2]);
